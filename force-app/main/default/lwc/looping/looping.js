9b506334b5234bbf950ae34758d3e805df216ad2
Vikram Raut <vikramraut21@gmail.com>
2022-11-09 14:10:29 +0530

    New car added

--- force-app/main/default/lwc/looping/looping.js
+++ force-app/main/default/lwc/looping/looping.js
@@ -1,5 +1,5 @@
 import { LightningElement } from 'lwc';
 
 export default class Looping extends LightningElement {
-    carList=["Ford","Audi","Maruti","Hundai","Mercedes"]
+    carList=["Ford","Audi","Maruti","Hundai","Mercedes","Tata"]
 }
